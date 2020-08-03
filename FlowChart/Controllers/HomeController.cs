86dbf041936fa6632ee5515b2621a1d72fa5a6e8
vinoth <vinothshankar40@gmail.com>
2020-08-03 06:20:25 +0530

    first commit

---
+++ FlowChart/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FlowChart.Models;
+using Newtonsoft.Json;
+
+namespace FlowChart.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            DevDemo DevDemo = new DevDemo();
+            string jsonFilePath = @"F:\Projects\FlowChart\FlowChart\Shapes\Shapes.json";
+            List<dynamic> Shapes = new List<dynamic>();
+            using (StreamReader r = new StreamReader(jsonFilePath))
+            {
+                string json = r.ReadToEnd();
+                Shapes = JsonConvert.DeserializeObject<List<dynamic>>(json);
+            }
+            DevDemo.Shapes = JsonConvert.SerializeObject(Shapes);
+            return View(DevDemo);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FlowChart/Models/DevDemo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlowChart.Models
+{
+    public class DevDemo
+    {
+        public string Shapes { get; set; }
+    }
+}
